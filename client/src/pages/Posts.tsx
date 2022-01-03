580be53877ddfbf3d7be2143826cea34c0d3011f
Janelle Parklee <parkleydondon@yahoo.com>
2022-01-02 17:13:15 -0800

    Added Post interface

--- client/src/pages/Posts.tsx
+++ client/src/pages/Posts.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Post } from '../common/interfaces/PostInterface'
 
-interface PostListProps {
-  posts: {
-    id: string,
-    content: string,
-    author: string ,   
-  }[]
+interface PostsProps {
+  posts: Post[]
 }
-const Posts: React.FC<PostListProps> = props => {
+
+const Posts: React.FC<PostsProps> = ({posts}: PostsProps) => {
  
   return (
     <div className="App">
       <h2>Posts</h2>
-      <ul>{props.posts.map(post => {
+      <ul>{posts.map(post => {
           return (<li key={post.id}>{post.content} by {post.author}</li>)
       })}</ul>
     </div>
