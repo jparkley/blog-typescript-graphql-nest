2568321c57c2e7ade544508b8b9b5817d3399497
Janelle Parklee <parkleydondon@yahoo.com>
2022-01-26 21:21:36 -0800

    Added form to add post

--- client/src/pages/Posts.tsx
+++ client/src/pages/Posts.tsx
@@ -1,33 +1,18 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import PostForm from '../components/posts/PostForm'
 import PostList from '../components/posts/PostList'
-import { useCreatePost } from '../hooks/useCreatePost'
 import { useGetposts } from '../hooks/useGetPosts'
 
-interface FormData {
-    title: string
-    content: string
-}
+
 
 const Posts:React.FC = () => {
     const posts = useGetposts()
-    const { register, handleSubmit } = useForm<FormData>()
-    const createPost = useCreatePost()
-
-    const onSubmit = handleSubmit(({title, content}) => {
-        console.log(title);
-        console.log(content);        
-        createPost({ variables: {input: {title, content}}})        
-    })
-    
+   
     return (
         <>
-            <form onSubmit={onSubmit}>
-                <h2>New Post</h2>
-                    <input type='text' {...register('title')} />
-                    <textarea {...register('content')} />                
-                    <button type='submit'>Submit</button>                
-            </form>
+            <h2>Add Post</h2>
+            <PostForm />
+            <h2>Posts</h2>
             <PostList posts={posts} />
         </>
     )
--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -3,7 +3,14 @@ const db = require('../db')
 const resolvers = {
     Query: {
         posts: () => db.posts.list()
-    }
+    },
+
+    // Mutation: {
+    //     createPost: (root, {title, content}) => {
+    //         const id = db.posts.create({title, content})
+    //         return db.posts.get(id)
+    //     } 
+    // }
 }
 
  
