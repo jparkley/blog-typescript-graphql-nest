5881dcce7825a831ff728096ae4641385ee996c9
Janelle Parklee <parkleydondon@yahoo.com>
2022-01-29 13:05:44 -0800

    Added basic validation

--- backend/src/blogposts/dto/create-blogpost.input.ts
+++ backend/src/blogposts/dto/create-blogpost.input.ts
@@ -1,13 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsAlpha, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateBlogpostInput {
+    @IsNotEmpty()
     @Field()
     title: string;
 
+    @IsNotEmpty()
     @Field()
     content: string;
-
+    
+    @IsNotEmpty()
+    @IsAlpha()
     @Field()
     author: string
 }
