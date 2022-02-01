ccf11f696ca2e731a1e1add9570b43dc9325d8d8
Janelle Parklee <parkleydondon@yahoo.com>
2022-01-31 23:14:22 -0800

    Added serverless server setup

---
+++ backend/src/serverless.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { AppModule } from './app.module';
+
+let server: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+  
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT')
+  //await app.listen(port);
+
+  await app.init()
+  const expressApp = app.getHttpAdapter().getInstance()
+  return serverlessExpress({ app: expressApp })
+}
+//bootstrap();
+export const hanlder: Handler = async (
+    event: any,
+    context: Context,
+    callback: Callback
+) => {
+    server = server ??  (await bootstrap())
+    return server(event, context, callback)
+}
