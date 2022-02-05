6260d99f46ff22ab2f4db839031bea6890f8e7e9
Janelle Parklee <parkleydondon@yahoo.com>
2022-02-05 12:20:49 -0800

    Updated TypeormModule to use ENV variables

---
+++ Deployment.md
@@ -0,0 +1,16 @@
+## Prepare code
+    # backend
+    # client
+
+## Set up AWS EC2 instance
+    New instance: Ubuntu 20.04, t2 micro(free tier)
+    => connect to it via SSH
+    => install nvm
+    => nvm i 16.13.1 (?)
+    => install packages
+    => exit and login int again
+    => install postgres
+    => get the db ready
+    => test app to see all the modules initialized fine
+
+## Set up AWS CICD
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,32 +3,45 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path/posix';
-import { getConnectionOptions } from 'typeorm';
 import { BlogpostsModule } from './blogposts/blogposts.module';
 
 // GraphQLModule.forRoot(configuration object)
 // entities:  ['dist/**/*.entity{.ts,.js}'],
-// TypeOrmModule.forRoot(),
+
+// Module order is impoertant as the ConfigModule load env variables.
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),     
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
       sortSchema: true
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () =>
-        Object.assign(await getConnectionOptions(), {
-          autoLoadEntities: true,
-        }),
+    TypeOrmModule.forRootAsync({      
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true
+      })      
+      // useFactory: async () =>
+      //   Object.assign(await getConnectionOptions(), {
+      //     autoLoadEntities: true,
+      //   }),
     }),
-    ConfigModule.forRoot({
-      isGlobal: true
-    }), 
     BlogpostsModule
   ],
   controllers: [],
   providers: [],
 })
+
+
 export class AppModule {}
+
