6c1f72efdf5affd0613fe80830cbbaae41b77394
Janelle Parklee <parkleydondon@yahoo.com>
2022-02-07 22:10:45 -0800

    Client build

--- client/src/common/apollo-client.ts
+++ client/src/common/apollo-client.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 // const GRAPHQL_ENDPOINT = 'http://localhost:4000/graphql'
 const host = window.location.hostname;
 const GRAPHQL_ENDPOINT = `http://${host}:4000/graphql/`;
-
+console.log("endpoint: ", GRAPHQL_ENDPOINT);
 const client = new ApolloClient({
   uri: GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
